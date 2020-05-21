fbe91aa09d513a92a64883ca22c0e5fe12125e98
Xan <xankuroi@gmail.com>
2020-05-21 13:48:57 -0400

    Data extraction

--- README.md
+++ README.md
@@ -1,24 +1,42 @@
-# characters
+# TRG Character Sheets
+
+This is the frontend for the TRG character sheets. The "database" is a number of Google sheets. W15 and W16 are currently hard-coded pages and do not pull from any sheets due to the closing of sheets API v3.
+
+Current frontend uses sheetjs to process an xlsx export of the workbook.
+
+## New Week Setup
+
+1. First, make a copy of the \[TEMPLATE\] TRG: Backend (W17+). Details about setting up the spreadsheet is in that spreadsheet.
+2. Publish sheet to web (File -> Publish to the web)
+3. Take the key from the publish link (https://docs.google.com/spreadsheets/d/e/THISISTHEKEY/pubhtml)
+4. In `vue.config.js`, add the new week to the `weeks` array.
+5. In `vue.config.js`, add two more entries in the pages directory, one for players and one for reapers. Everything should be the same as the other entries except for the week numbers and the `publishKey`
+6. Push changes to master
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,194 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="keyPresent">
+      This is a real page
+      <div v-for="(sheet, index) in processedSheets" :key="index">
+        {{ sheet }}
+      </div>
+      <Tab />
+      <TabContent />
+    </div>
+    <div v-else class="list">
+      <template v-for="week in weeks">
+        <a
+          :href="origin + '/' + week + '/players.html'"
+          :key="week + '-player'"
+        >
+          {{ week }} Players
+        </a>
+        <a
+          :href="origin + '/' + week + '/reapers.html'"
+          :key="week + '-reaper'"
+        >
+          {{ week }} Reapers
+        </a>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tab from "./components/Tab.vue";
+import TabContent from "./components/TabContent.vue";
+import XLSX from "xlsx";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Tab,
+    TabContent
+  },
+  data() {
+    return {
+      keyPresent: !!window.publishKey,
+      sheetURL: `https://docs.google.com/spreadsheets/d/e/${window.publishKey}/pub?output=xlsx`,
+      weeks: window.weeks,
+      origin: "",
+      rawData: {},
+      coordRegex: new RegExp(/[a-z]+|\d+/gi)
+    };
+  },
+  computed: {
+    pinData() {
+      return this.keyData(this.rawData.Pins, "Name");
+    },
+    threadData() {
+      return this.keyData(this.rawData.Threads, "Name");
+    },
+    foodData() {
+      return this.keyData(this.rawData.Foods, "Name");
+    },
+    sheetConfig() {
+      return this.toJSON(this.rawData.Config);
+    },
+    ignoredSheets() {
+      const partialMatch = this.sheetConfig[1].UNSEEN.split(",")
+        .map(str => str.trim())
+        .join("|");
+      const exactMatch = this.sheetConfig[0].UNSEEN.split(",")
+        .map(str => str.trim())
+        .join("$|^");
+      return new RegExp(`${partialMatch}|^${exactMatch}$`);
+    },
+    dataCoords() {
+      let hsh = {};
+      this.sheetConfig.forEach(datum => {
+        if (datum.FIELD) {
+          hsh[datum.FIELD] = datum.COORD;
+        }
+      });
+      return hsh;
+    },
+    processedSheets() {
+      let hsh = {};
+      Object.keys(this.rawData || {})
+        .filter(key => !this.ignoredSheets.test(key))
+        .forEach(key => (hsh[key] = this.processSheet(this.rawData[key])));
+      return hsh;
+    }
+  },
+  methods: {
+    toJSON(sheet) {
+      return XLSX.utils.sheet_to_json(sheet);
+    },
+    processCoord(coord, rowOffset, colOffset) {
+      let [col, row] = coord.match(this.coordRegex);
+      // WARNING: doesn't handle Z -> AA transition
+      if (colOffset) {
+        col = String.fromCharCode(col.charCodeAt(0) + colOffset);
+      }
+      if (rowOffset) {
+        row = Number(row) + rowOffset;
+      }
+      return col + row;
+    },
+    processField(sheet, field) {
+      if (!field.LEN) {
+        // standard processing
+        return sheet[field.COORD].w;
+      }
+      // special processing
+      let data;
+      const num = Number(field.LEN);
+      const coord = field.COORD;
+
+      if (num) {
+        // numeric LEN, so this is inventory
+        const nOffset = Number(field.N);
+        const dOffset = Number(field.D);
+        data = [];
+        for (let i = 0; i < num; i++) {
+          const name = sheet[this.processCoord(coord, i, 0)];
+          if (name) {
+            const datum = { name: name.w };
+            if (nOffset) {
+              const n = sheet[this.processCoord(coord, i, nOffset)];
+              if (n) {
+                datum.n = n.v;
+              }
+            }
+            if (dOffset) {
+              const d = sheet[this.processCoord(coord, i, dOffset)];
+              if (d) {
+                datum.d = d.v;
+              }
+            }
+            data.push(datum);
+          }
+        }
+      } else {
+        // non-numeric LEN, so these are stats
+        data = {
+          raw: sheet[coord].v,
+          misc: sheet[this.processCoord(coord, Number(field.N), 0)].v,
+          trained: sheet[this.processCoord(coord, Number(field.D), 0)].v
+        };
+        if (field.NAME === "HP") {
+          data.current = data.trained;
+        }
+      }
+      return data;
+    },
+    processSheet(sheet) {
+      let data = { Noise: {} };
+      this.sheetConfig.forEach(field => {
+        const fieldData = this.processField(sheet, field);
+
+        if (field.NAME.startsWith("Noise")) {
+          data.Noise[field.NAME.substring(6)] = fieldData;
+        } else {
+          data[field.NAME] = fieldData;
+        }
+      });
+      return data;
+    },
+    keyData(sheet, key) {
+      let data = {};
+      this.toJSON(sheet)
+        .filter(row => row[key])
+        .forEach(row => (data[row[key]] = row));
+      return data;
+    },
+    loadData(url) {
+      fetch(url)
+        .then(resp => {
+          if (!resp.ok) throw new Error("Failed to load data.");
+          return resp.arrayBuffer();
+        })
+        .then(buffer => {
+          const arrBuffer = new Uint8Array(buffer);
+          this.rawData = XLSX.read(arrBuffer, { type: "array" }).Sheets;
+        });
+    }
+  },
+  mounted() {
+    this.origin = window.location.origin;
+    // Fetch data
+    if (this.keyPresent) {
+      this.loadData(this.sheetURL);
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -21,8 +196,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.list a {
+  display: block;
+  margin-left: 20px;
+}
+
+a {
+  color: #2c3e50;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #5fbde5;
+  font-weight: bold;
 }
 </style>
