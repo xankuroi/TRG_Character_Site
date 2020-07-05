f10f789cb48f8f5dfbd71041ab2050181c4bfe68
Xan <xankuroi@gmail.com>
2020-07-05 19:01:04 -0400

    Update config for W17

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,25 @@ module.exports = {
     index: {
       entry: "src/index.js",
       title: "The Reaper's Game Character Sheets",
-      weeks: ["W17"]
+      weeks: ["test"],
+    },
+    "Test Page": {
+      entry: "src/main.js",
+      title: "Test Page",
+      filename: "test/index.html",
+      playerKey:
+        "2PACX-1vSWTyHAq5Ek0l3DIxWx8my7nBq4QPEBYeJ59NqZPYKUfuBBDY3JCabXwCZo4Sf7B0fxVmvLzd5VT_CQ",
+      reaperKey:
+        "2PACX-1vSWTyHAq5Ek0l3DIxWx8my7nBq4QPEBYeJ59NqZPYKUfuBBDY3JCabXwCZo4Sf7B0fxVmvLzd5VT_CQ",
     },
     "W17 Characters": {
       entry: "src/main.js",
       title: "W17 Characters",
       filename: "W17/index.html",
       playerKey:
-        "2PACX-1vSWTyHAq5Ek0l3DIxWx8my7nBq4QPEBYeJ59NqZPYKUfuBBDY3JCabXwCZo4Sf7B0fxVmvLzd5VT_CQ",
+        "2PACX-1vQ9mahGpwePTKAx8tV8qAM43iY-2VgxPFSG_gL-seTTifV-HNxcOrHm8FYvm8a4gU02HLULbnCwvzhp",
       reaperKey:
-        "2PACX-1vSWTyHAq5Ek0l3DIxWx8my7nBq4QPEBYeJ59NqZPYKUfuBBDY3JCabXwCZo4Sf7B0fxVmvLzd5VT_CQ"
-    }
-  }
+        "2PACX-1vQykYWNZ5q269yQrpIj3Ai6wJ2HM2qvu_1CC53LDl-gJOfki72Vq28DYP-mcz-6RXbKBnNrSlflcugI",
+    },
+  },
 };
